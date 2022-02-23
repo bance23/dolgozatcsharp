2244a860188d7b4bf25f6821900701a70c7df666
Bence Benedek Balogh <2021290@dszcberegszaszi.hu>
2022-02-23 11:21:07 +0100

    update

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Írja be az intervallum kezdőértékét:");
+            int intmin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Írja be az intervallum végértékét:");
+            int intmax = Convert.ToInt32(Console.ReadLine());
+            int aktszam = intmin - 1;
+
+            for (int i = intmin; i <= intmax; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
